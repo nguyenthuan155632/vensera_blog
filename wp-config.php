9fb70da2aa788f7191362e101070a2150d4e45a6
root <root@ip-172-31-24-139.ap-northeast-1.compute.internal>
2017-04-13 02:30:22 +0000

    final

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('WP_CACHE', true); //Added by WP-Cache Manager
-define( 'WPCACHEHOME', '/var/www/vensera_blog/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
+define( 'WPCACHEHOME', '/var/www/html/vensera_blog/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
 define('DB_NAME', 'vensera_blog');
 
 /** MySQL database username */
